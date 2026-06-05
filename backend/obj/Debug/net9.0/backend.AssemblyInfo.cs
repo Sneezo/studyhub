f36a1bfec9dc82e82484fd6ce373ae5e537d71a1
unknown <Administrator@BLIX.HFT>
2026-06-05 15:24:32 +0200

    Connected SQL server to project

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d1413585b81d1b53ae9715f113244a3de081cce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd283315c2c652bc0ef2721a984a0ded53be7b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
