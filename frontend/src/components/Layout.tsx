5a179b55a031366593359fbe76dee0a59702f121
Sneezo <vegard-blix.abrahamsen@hotmail.com>
2026-06-09 09:05:29 +0200

    Added FSMO page link

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -10,6 +10,9 @@ export function Layout() {
           <Link to="/">Home</Link>
           <Link to="/news">News</Link>
           <Link to="/cms">CMS</Link>
+          <a href="/fsmo.html" target="_blank" rel="noreferrer">
+            FSMO
+          </a>
         </nav>
       </header>
 
